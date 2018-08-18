728cc84a52553cc74bf2e3f83e271c14ffcd58d4
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-08-18 11:29:21 -0300

    Add base template.

--- ui.R
+++ ui.R
@@ -1,25 +1,30 @@
 library(shiny)
 library(plotly)
 # Define UI
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Lupa na Toga"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      uiOutput("orgao"),
-      uiOutput("mes")
-    ),
-    
-    # Show a plot of the generated graph
-    mainPanel(
-      plotlyOutput("pointsPlot"),
-      plotlyOutput("linesPlot"),
-      sunburstOutput("sumburstPlot"),
-      textOutput("selection")
-    )
-  )
 
-  
+shinyUI(bootstrapPage(
+  htmlTemplate("index.html", name = "1")
 ))
+
+# shinyUI(fluidPage(
+#   
+#   # Application title
+#   titlePanel("Lupa na Toga"),
+#   
+#   sidebarLayout(
+#     sidebarPanel(
+#       uiOutput("orgao"),
+#       uiOutput("mes")
+#     ),
+#     
+#     # Show a plot of the generated graph
+#     mainPanel(
+#       plotlyOutput("pointsPlot"),
+#       plotlyOutput("linesPlot"),
+#       sunburstOutput("sumburstPlot"),
+#       textOutput("selection")
+#     )
+#   )
+# 
+#   
+# ))
