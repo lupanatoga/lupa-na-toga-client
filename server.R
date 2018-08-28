8c5fd88fa05365c12d3c2400b608959b59c63b05
Vinicius Brandao <vinicius.brandao.araujo@ccc.ufcg.edu.br>
2018-08-28 09:13:09 -0300

    Gráfico do tempo concertados, sunburstR concertado pelos valores em Bilhão

--- server.R
+++ server.R
@@ -7,9 +7,11 @@ library(jsonlite)
 library(readr)
 library(zoo)
 library(htmltools)
+library(lubridate)
 
 data = read_csv("salarios_tratados_com_mes.csv")
-
+data$mes_ano_referencia <- paste0("01/",data$mes_ano_referencia )
+data$mes_ano_referencia <- dmy(data$mes_ano_referencia)
 salarios_t = data
 salarios_t[is.na(salarios_t)] = 0
 
@@ -130,20 +132,19 @@ shinyServer(function(input, output) {
     s <- event_data("plotly_click", source = "juiz")
     if(length(s)) {
       juiz = sumario[s[["pointNumber"]]+1,]
-      salarios_um = data %>% filter(nome == juiz$nome) %>%  select(total, mes)
+      salarios_um = data %>% filter(nome == juiz$nome) %>% group_by(mes_ano_referencia) %>%  summarise(total = sum(rendimento_liquido))
       print(salarios_um)
       plot_ly(data = salarios_um,
               y = ~ total,
-              x = ~ mes,
+              x = ~ mes_ano_referencia,
               type = "scatter",
               source = "temporal",
-              mode = "lines"
-      )
+              mode = "lines")
     } else {
-      salarios_um = data %>% group_by(mes) %>%  summarise(total = sum(rendimento_liquido))
+      salarios_um = data %>% group_by(mes_ano_referencia) %>%  summarise(total = sum(rendimento_liquido))
       plot_ly(data = salarios_um,
               y = ~ total,
-              x = ~ mes,
+              x = ~ mes_ano_referencia,
               type = "scatter",
               source = "temporal",
               mode = "lines")
@@ -155,11 +156,36 @@ shinyServer(function(input, output) {
   })
   
   render_sunburst = function(data) {
-    d <- data.frame(group = c("subsidio","direitos_pessoais-abono_de_permanencia", "direitos_pessoais-subsidio_outra1", "indenizacoes-ajuda_de_custo","indenizacoes-indenizacoes_outros","direitos_eventuais-indenizacao_de_ferias","direitos_eventuais-abono_contitucional_de_1_3_de_ferias","direitos_eventuais-antecipacao_de_ferias","direitos_eventuais-gratificacao_natalina","direitos_eventuais-antecipacao_de_gratificacao_natalina","direitos_eventuais-substituicao","direitos_eventuais-gratificacao_por_exercicio_cumulativo","direitos_eventuais-gratificacao_por_encargo_curso_concurso","direitos_eventuais-pagamento_em_retroativos","direitos_eventuais-jeton", "indenizacoes-auxilio-auxilio_alimentacao", "indenizacoes-auxilio-auxilio_pre_escolar", "indenizacoes-auxilio-auxilio_saude", "indenizacoes-auxilio-auxilio_natalidade", "indenizacoes-auxilio-auxilio_moradia"),
+    custom.message = "function (d) {
+    root = d;
+    while (root.parent) {
+    root = root.parent
+    }
+    p = (100*d.value/root.value).toPrecision(3);
+    if(root.value >= 1000000000) {
+    y =   Math.round(root.value/1000000000) + 'B';
+    }
+    x = d.value
+    if(x  >= 1000000000) {
+    x =   Math.round(x/1000000000) + 'B';
+    }
+    
+    if(x < 1000000000 && x >= 1000000 ) {
+    x = Math.round(x/1000000) + 'M';
+    }
+    
+    
+    if(x < 1000000) {
+    x =  Math.round(x/1000) + 'K';
+    }
+    msg = p +' %<br/>'+x+' de '+ y;
+    return msg;
+    }"
+    d <- data.frame(group = c("Salarios","direitos_pessoais-abono_de_permanencia", "direitos_pessoais-subsidio_outra1", "indenizacoes-ajuda_de_custo","indenizacoes-indenizacoes_outros","direitos_eventuais-indenizacao_de_ferias","direitos_eventuais-abono_contitucional_de_1_3_de_ferias","direitos_eventuais-antecipacao_de_ferias","direitos_eventuais-gratificacao_natalina","direitos_eventuais-antecipacao_de_gratificacao_natalina","direitos_eventuais-substituicao","direitos_eventuais-gratificacao_por_exercicio_acumulativo","direitos_eventuais-gratificacao_por_encargo_curso_concurso","direitos_eventuais-pagamento_em_retroativos","direitos_eventuais-jeton", "indenizacoes-auxilio-auxilio_alimentacao", "indenizacoes-auxilio-auxilio_pre_escolar", "indenizacoes-auxilio-auxilio_saude", "indenizacoes-auxilio-auxilio_natalidade", "indenizacoes-auxilio-auxilio_moradia"),
                     value = c(sum(data$subsidio), sum(data$abono_de_permanencia), sum(data$subsidio_outra1),sum(data$ajuda_de_custo),sum(data$indenizacoes_outra1 + data$indenizacoes_outra2 + data$indenizacoes_outra3),sum(data$indenizacao_de_ferias),sum(data$abono_contitucional_de_1_3_de_ferias) ,sum(data$antecipacao_de_ferias) ,sum(data$gratificacao_natalina),sum(data$antecipacao_de_gratificacao_natalina),sum(data$substituicao),sum(data$gratificacao_por_exercicio_cumulativo),sum(data$gratificacao_por_encargo_curso_concurso),sum(data$pagamento_em_retroativos),sum(data$jeton),sum(data$auxilio_alimentacao),sum(data$auxilio_pre_escolar), sum(data$auxilio_saude), sum(data$auxilio_natalidade),sum(data$auxilio_moradia)))
     
     
-    sunburst(d,count = TRUE) 
+    sunburst(d,count = TRUE,explanation = custom.message) 
   }
   
   output$fistPlot <- renderSunburst({
@@ -175,7 +201,7 @@ shinyServer(function(input, output) {
       juiz = sumario[s[["pointNumber"]]+1,]
       
       if(length(s2)) {
-        salarios_temp = data %>%filter(mes == s2[["x"]])
+        salarios_temp = data %>%filter(mes_ano_referencia == s2[["x"]])
       } else {
         salarios_temp = data
       }
